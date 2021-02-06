cb7cea601a8b18bbd5cf0d69f63f4de50fb9d2b6
PrajjwalGangwar <prajjwalsingh.singh@gmail.com>
2021-02-06 16:33:34 +0530

    searchBar.dart added with icon button of search on home

--- lib/design/home.dart
+++ lib/design/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:virtual_auction/design/ProfilePage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:virtual_auction/widgets/searchBar.dart';
 import '../widgets/Home_CardView.dart';
 import 'package:virtual_auction/development/FetchProfile.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -54,10 +55,15 @@ class HomeState extends State<Home>{
           actions: [
             IconButton(
               color: Colors.black,
+              icon: Icon(Icons.search_outlined),
+              onPressed: () async {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => SearchBarDemoApp()));
+              },
+            ),
+            IconButton(
+              color: Colors.black,
               icon: Icon(Icons.live_help),
               onPressed: () async {
-                // await FirebaseAuth.instance.signOut();
-                // Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
                 showAlertDialog(context);
               },
             ),
---
+++ lib/widgets/searchBar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_search_bar/flutter_search_bar.dart';
+
+class SearchBarDemoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new SearchBarDemoHome();
+  }
+}
+
+class SearchBarDemoHome extends StatefulWidget {
+  @override
+  _SearchBarDemoHomeState createState() => new _SearchBarDemoHomeState();
+}
+
+class _SearchBarDemoHomeState extends State<SearchBarDemoHome> {
+  SearchBar searchBar;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  AppBar buildAppBar(BuildContext context) {
+    return new AppBar(
+        actions: [searchBar.getSearchAction(context)]);
+  }
+
+  void onSubmitted(String value) {
+    setState(() => _scaffoldKey.currentState
+        .showSnackBar(new SnackBar(content: new Text('You wrote $value!'))));
+  }
+
+  _SearchBarDemoHomeState() {
+    searchBar = new SearchBar(
+        inBar: false,
+        buildDefaultAppBar: buildAppBar,
+        setState: setState,
+        onSubmitted: onSubmitted,
+        onCleared: () {
+          print("cleared");
+        },
+        onClosed: () {
+          print("closed");
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: searchBar.build(context),
+      key: _scaffoldKey,
+      body: new Center(
+          child: new Text("Don't look at me! Press the search button!")),
+    );
+  }
+}
